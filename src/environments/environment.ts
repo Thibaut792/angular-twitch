a63d9b4be6443a16f818a9cdbcbe4927e983c6e6
ThibautFiquet <thibautfiquet792@gmail.com>
2023-04-12 09:55:03 +0200

    bug player

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,9 +13,9 @@ export const environment = {
   production: true,
   api: { ...apiConfig, ...oAuthConfig },
   twitch: {
-    streamPlayerUrl: 'https://player.twitch.tv/?parent=valarep-2023.netlify.app&channel=',
-    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=valarep-2023.netlify.app&clip=',
-    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=valarep-2023.netlify.app&video=',
-    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=valarep-2023.netlify.app',
+    streamPlayerUrl: 'https://player.twitch.tv/?parent=angular-app-twitch.netlify.app&channel=',
+    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=angular-app-twitch.netlify.app&clip=',
+    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=angular-app-twitch.netlify.app&video=',
+    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=angular-app-twitch.netlify.app',
   }
 };
